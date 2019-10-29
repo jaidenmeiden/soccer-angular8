ce323c08e1f1844771aac5bb0101e3bf26d18a06
DLI <sistemas@dli.com.co>
2019-10-29 11:16:57 -0500

    Se ajusta configuración para scss incluyendo routing.

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to soccer-angular8!');
+    expect(page.getTitleText()).toEqual('soccer-angular8 app is running!');
   });
 
   afterEach(async () => {

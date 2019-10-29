1cada316bbc0f6822f8f9738988b92d7fe33879c
DLI <sistemas@dli.com.co>
2019-10-29 12:33:55 -0500

    Se crea y configura componente Team

---
+++ src/app/components/team-table/team-table.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { TeamService, TeamsTableHeaders } from '../../services/team.service';
+import { Observable } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { Team } from '../../interfaces/team';
+import {Countries} from "../../global/variables";
+
+@Component({
+  selector: 'app-team-table',
+  templateUrl: './team-table.component.html',
+  styleUrls: ['./team-table.component.scss']
+})
+export class TeamTableComponent implements OnInit {
+
+  public teams$: Observable<Team[]>;
+  public tableHeaders = TeamsTableHeaders;
+
+  constructor(private teamService: TeamService) {
+  }
+
+  ngOnInit() {
+    this.teams$ = this.teamService.getTeams();
+    this.teamService
+      .getTeams()
+      .pipe(take(1))
+      .subscribe(teams => {
+        if (teams.length === 0) {
+          const team: Team = {
+            name: 'Los agricultors!',
+            country: Countries.Colombia,
+            players: null
+          };
+          this.teamService.addTeam(team);
+        }
+      });
+  }
+}

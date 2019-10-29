a3d8d8550f7f2b37c4b09b7649088016ef3fafaf
DLI <sistemas@dli.com.co>
2019-10-29 11:31:45 -0500

    Se configura Firebase en environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCsd_eZsJIMX88yU1R3YyF20y_q9ZORA2A",
+    authDomain: "soccerangular8.firebaseapp.com",
+    databaseURL: "https://soccerangular8.firebaseio.com",
+    projectId: "soccerangular8",
+    storageBucket: "soccerangular8.appspot.com",
+    messagingSenderId: "115209692939",
+    appId: "1:115209692939:web:88a1f216e7b3f9d2b2640e",
+    measurementId: "G-CW3R3WDHDT"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCsd_eZsJIMX88yU1R3YyF20y_q9ZORA2A",
+    authDomain: "soccerangular8.firebaseapp.com",
+    databaseURL: "https://soccerangular8.firebaseio.com",
+    projectId: "soccerangular8",
+    storageBucket: "soccerangular8.appspot.com",
+    messagingSenderId: "115209692939",
+    appId: "1:115209692939:web:88a1f216e7b3f9d2b2640e",
+    measurementId: "G-CW3R3WDHDT"
+  }
 };
 
 /*

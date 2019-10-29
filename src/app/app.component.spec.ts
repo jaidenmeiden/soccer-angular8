49b331f08b24effb34feeeb0013cf59f5374e79d
DLI <sistemas@dli.com.co>
2019-10-29 11:17:25 -0500

    Se modifican archivos del app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -22,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('soccer-angular8');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to soccer-angular8!');
+    expect(compiled.querySelector('.content span').textContent).toContain('soccer-angular8 app is running!');
   });
 });

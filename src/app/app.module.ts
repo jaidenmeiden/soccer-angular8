a06fc71db1639b1a04323e2f28e9fd97132e2929
DLI <sistemas@dli.com.co>
2019-10-29 12:33:29 -0500

    Se implementan los componentes Player y Team y los estilos globales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,14 @@ import { AppComponent } from './app.component';
 import {environment} from "../environments/environment";
 import {PlayerService} from "./services/player.service";
 import {TeamService} from "./services/team.service";
+import { TeamTableComponent } from './components/team-table/team-table.component';
+import { PlayerTableComponent } from './components/player-table/player-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamTableComponent,
+    PlayerTableComponent
   ],
   imports: [
     BrowserModule,
